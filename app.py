48bb4fff5274ab58071c498c3b79a3ff9c64698f
a17822228533-hash <a17822228533@icloud.com>
2026-08-24 01:50:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from flask_cors import CORS
 import copy
 import json, os, time, random, string, hashlib, sqlite3
 from datetime import datetime, timezone, timedelta as _td
-
+# 初始化数据库
+import sqlite3
+DB_PATH = DATA_DIR + '/mochi.db'
+if not os.path.exists(DB_PATH):
+    import init_db
+    print("数据库初始化完成")
 app = Flask(__name__)
 CORS(app)
 
